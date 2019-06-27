7a5d12a93a89cb9bacc00a8dfa64ceb7bfee6e56
Innan Plínio Rangel Amorim <45242384+innanplinio@users.noreply.github.com>
2019-06-27 02:11:22 -0300

    att

--- tp1/GraphTheoryJava (2)/src/graphtheoryjava/Main.java
+++ tp1/GraphTheoryJava (2)/src/graphtheoryjava/Main.java
@@ -6,17 +6,21 @@ import graphtheoryjava.algorithms.Search;
 import graphtheoryjava.util.Reader;
 import graphtheoryjava.util.Graph;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
+            ArrayList<Double> a = new ArrayList<>();
+            ArrayList<Double> b = new ArrayList<>();
+            ArrayList<Double> c2 = new ArrayList<>();
             System.out.println("Graph file: ");
             Scanner in = new Scanner(System.in);
             String fileName = in.nextLine();
             Reader reader = new Reader();
-
             Graph graph = reader.readFile(fileName);
 
 //            System.out.println(graph.adjList);
@@ -40,18 +44,41 @@ public class Main {
 //                    + search.topologicalSort());
             CaminhoMinimo c = new CaminhoMinimo();
             System.out.println();
+            Random r = new Random();
 
-            long time = System.nanoTime();
-            c.Dijkstra(graph, 0, 100);
-            System.out.println("Tempo de Execução Dijkstra: " + (System.nanoTime() - time) + "\n");
+            //Início do cálculo de caminho mínimo
+            int vert = 3353; //qtd de vertices no grafo            
+            Double time2;
+            for (int i = 0; a.size() != 10; i++) {
 
-            time = System.nanoTime();
-            c.BellmanFord(graph, 0, 100);
-            System.out.println("Tempo de Execução Bellman-Ford: " + (System.nanoTime() - time) + "\n");
+                //Sorteio de Vértice De início e fim
+                int s, d;
+                s = r.nextInt(vert);
+                d = r.nextInt(vert);
+                while (s == d) {
+                    d = r.nextInt(vert);
+                }
+                System.out.println("\nS: "+s+" D: "+d+"\n");
 
-            time = System.nanoTime();
-            c.FloydWarshall(graph, 0, 100);
-            System.out.println("Tempo de Execução Floyd-Warshall: " + (System.nanoTime() - time) + "\n");
+                long time = System.nanoTime();
+                c.Dijkstra(graph, s, d);
+                time2 = (double) (System.nanoTime() - time) / 1000000000;
+                a.add(time2);
+                System.out.println("Tempo de Execução Dijkstra: " + time2 + "\n");
+
+                time = System.nanoTime();
+                c.BellmanFord(graph, s, d);
+                time2 = (double) (System.nanoTime() - time) / 1000000000;
+                b.add(time2);
+                System.out.println("Tempo de Execução Bellman-Ford: " + time2 + "\n");
+
+                time = System.nanoTime();
+                c.FloydWarshall(graph, s, d);
+                time2 = (double) (System.nanoTime() - time) / 1000000000;
+                c2.add(time2);
+                System.out.println("Tempo de Execução Floyd-Warshall: " + time2 + "\n");
+            }
+            System.out.println("Tempo dijkstra: " + a.toString() + "\nTempo Bellman-ford: " + b.toString() + "\nTempo Floyd-Warshall: " + c2.toString() + "\n\n");
 
         } catch (IOException e) {
             e.printStackTrace();
